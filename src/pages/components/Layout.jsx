c04b3174ca4e9105ce140fa1a53061b67d33568d
Chiamaka Ugoji <mychiamaka@gmail.com>
2023-05-01 13:32:08 +0100

    Update Layout.jsx

--- src/pages/components/Layout.jsx
+++ src/pages/components/Layout.jsx
@@ -1,18 +1,30 @@
 import { Icon } from "@iconify/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
   return (
     <div>
       <main className="bg-[#F6F8FE] h-full">{children}</main>
       <div className="footer self-end ">
-        <div className="  flex justify-between bg-white rounded-full p-3 opacity-30">
+        <div className="  flex justify-between bg-white rounded-full p-3 text-[#D3D3D3]">
           <Link href="/">
-            <Icon icon="mdi:home-variant" width={30} />
+            <Icon
+              icon="mdi:home-variant"
+              width={30}
+              className={`${router.pathname == "/" ? "text-[#6672BA]" : ""}`}
+            />
           </Link>
           <Link href="/stats">
-            <Icon icon="ion:stats-chart" width={30} />
+            <Icon
+              icon="ion:stats-chart"
+              width={30}
+              className={`${
+                router.pathname == "/stats" ? "text-[#6672BA]" : ""
+              }`}
+            />
           </Link>
           <Link href="/">
             <Icon icon="ic:round-people-alt" width={30} />
